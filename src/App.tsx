ab56f7576f16cf48395c411a77e9c8e8219234ba
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-03-23 17:24:33 -0400

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+/* global chrome */
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import TopComment from "./components/TopComment";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  // useEffect(() => {
+  //   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  //     let url = tabs[0].url;
+  //     console.log(url);
+  //   });
+  // }, []);
+
+  return (
+    <div className="App">
+      <h1>WWW Community Notes</h1>
+      <TopComment />
+      <TopComment />
+      <TopComment />
+      <TopComment />
+      <TopComment />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TopComment.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./component_styles/TopComment.css";
+
+const TopComment = () => {
+  return (
+    <div className="top_comment_container">
+      <h3 className="header">Some readers wanted to add context</h3>
+      <p className="content">
+        Although this and that, therefore there could be no that and this bruh
+        huh?
+      </p>
+    </div>
+  );
+};
+
+export default TopComment;
