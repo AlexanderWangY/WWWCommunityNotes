e32bc55082e5fce9b5a46bf204c06677cc6dfef7
h1divp <71522316+h1divp@users.noreply.github.com>
2024-03-24 04:14:58 -0400

    completed note creation

--- src/actions/createNote.ts
+++ src/actions/createNote.ts
@@ -2,10 +2,10 @@ import { doc, setDoc, collection } from 'firebase/firestore';
 import { Note } from '../types/note';
 import { db } from '../utilities/firebaseInit'
 
-export const createNote = async (url: string) => {
+export const createNote = async (url: string, userID: string, noteContent: string) => {
   const data: Note = {
     userID: '',
-    noteContent: '',
+    noteContent: noteContent,
     votes: 0,
     voters: []
   }
