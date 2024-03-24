005b1557c76e62d5d783a857beec1517e8498847
AlexanderWangY <alexanderwang@ufl.edu>
2024-03-23 21:56:26 -0400

    added some fake data and note text box

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,57 @@ import logo from "./logo.svg";
 import "./App.css";
 import TopComment from "./components/TopComment";
 import NormalComment from "./components/NormalComment";
+import { fakeData } from "./test";
 
-function App() {
-  // useEffect(() => {
-  //   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-  //     let url = tabs[0].url;
-  //     console.log(url);
-  //   });
-  // }, []);
+interface NoteData {
+  userID: string;
+  noteContent: string;
+  votes: number;
+  voters: { userID: string; voteCount: number }[];
+}
+
+
+const App = () => {
+  const [topNote, setTopNote] = React.useState<NoteData>();
+  const [range, setRange] = React.useState(1);
+  const [writing, setWriting] = React.useState(false);
+
+  useEffect(() => {
+    fakeData.sort((a, b) => b.votes - a.votes);
+    setTopNote(fakeData[0]);
+    setRange(5);
+  }, []);
 
   return (
     <div className="App">
       <h1 className="main_header">WWW Community Notes</h1>
-      <TopComment
-        username="Someguyagain"
-        content="Est provident non perspiciatis consequatur accusamus omnis sint omnis.
-          Quos omnis non magni eligendi doloremque error. Dolorem officiis
-          accusamus aut. Deleniti aspernatur ex voluptas. Laboriosam quasi sunt
-          porro mollitia mollitia. Et quam adipisci tempore et."
-      />
+      {topNote && (
+        <TopComment
+          username={topNote.userID}
+          content={topNote.noteContent}
+        />
+      )}
       <h2 className="other_note_divider">Other notes</h2>
-      <NormalComment
-        username="Someguy123"
-        content="Est provident non perspiciatis consequatur accusamus omnis sint omnis.
-          Quos omnis non magni eligendi doloremque error. Dolorem officiis
-          accusamus aut. Deleniti aspernatur ex voluptas. Laboriosam quasi sunt
-          porro mollitia mollitia. Et quam adipisci tempore et."
-      />
-      <NormalComment
-        username="AlexWala"
-        content="Est provident non perspiciatis consequatur accusamus omnis sint omnis.
-          Quos omnis non magni eligendi doloremque error. Dolorem officiis
-          accusamus aut. Deleniti aspernatur ex voluptas. Laboriosam quasi sunt
-          porro mollitia mollitia. Et quam adipisci tempore et."
-      />
-      <button className="add_note_button">Add your own thoughts</button>
+      {fakeData.slice(1, range).map((note) => {
+        return (
+          <NormalComment
+            key={note.userID}
+            username={note.userID}
+            content={note.noteContent}
+          />
+        );
+      })}
+      { range < fakeData.length ? (
+        <a className="render_more_button" onClick={() => setRange(range + 3)}>Click to render more</a>
+      ) : null}
+      {writing ? (
+        <div className="write_note_div">
+          <textarea className="write_note_textarea" />
+          <button className="submit_note_button">Submit</button>
+        </div>
+      ) : (
+        <button onClick={() => setWriting(true)} className="add_note_button">Add your own thoughts</button>
+      )}
     </div>
   );
 }
